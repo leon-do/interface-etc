ed77c3e3484029603a5f64cce95b5ebec9b9175d
leon-do <leondo123@gmail.com>
2024-05-10 12:58:06 -0400

    update privacy policy settings

--- src/components/WalletModal/PrivacyPolicyNotice.tsx
+++ src/components/WalletModal/PrivacyPolicyNotice.tsx
@@ -11,17 +11,17 @@ const LastUpdatedText = styled.span`
   color: ${({ theme }) => theme.neutral3};
 `
 
-const LAST_UPDATED_DATE = '6.7.23'
+const LAST_UPDATED_DATE = '5.1.24'
 
 export default function PrivacyPolicyNotice() {
   return (
     <ThemedText.BodySmall color="neutral2">
       <Trans>By connecting a wallet, you agree to ETCswap&apos;s</Trans>{' '}
-      <StyledLink href="https://etcswap.org/terms-of-service/">
+      <StyledLink href="https://docs.etcswap.org/legal/terms-of-service/">
         <Trans>Terms of Service</Trans>{' '}
       </StyledLink>
       <Trans>and consent to its</Trans>{' '}
-      <StyledLink href="https://etcswap.org/privacy-policy">
+      <StyledLink href="https://docs.etcswap.org/legal/privacy-policy">
         <Trans>Privacy Policy.</Trans>
       </StyledLink>
       <LastUpdatedText>
